acd48797cf01a12faba3175f34df824ab6f5ad5d
10high <99827666+10high@users.noreply.github.com>
2023-01-16 19:21:40 +0100

    adds location api

--- src/app.js
+++ src/app.js
@@ -1,31 +1,45 @@
 /* import { apiTime } from "./api/time";
  */
 
+/* import { apiKeys } from "../_apiKeys/apiKey"; */
 
-const apiTimeData = {};
+const clockData = {};
 
 const apiTime = async () => {
     try {
         const resolved = await fetch(`https://worldtimeapi.org/api/ip`);
         const data = await resolved.json();
         const time = new Date(data.datetime);
-        apiTimeData.hours = time.getHours();
-        apiTimeData.minutes = time.getMinutes();
-        apiTimeData.timezoneFull = data.timezone;
-        apiTimeData.timezoneAbbreviation = data.abbreviation;
-        apiTimeData.weekNumber = data.week_number;
-        apiTimeData.dayOfYear = data.day_of_year;
-        apiTimeData.dayOfWeek = data.day_of_week;
+        clockData.hours = time.getHours();
+        clockData.minutes = time.getMinutes();
+        clockData.timezoneFull = data.timezone;
+        clockData.timezoneAbbreviation = data.abbreviation;
+        clockData.weekNumber = data.week_number;
+        clockData.dayOfYear = data.day_of_year;
+        clockData.dayOfWeek = data.day_of_week;
     }
     catch (error) {
         console.log("this is the APITime fetch error: ", error);
     }
 }
 
+const apiLocation = async () => {
+    const apiKey = apiKeys.ipBase;
+    try {
+        const resolved = await fetch(`https://api.ipbase.com/v2/info?apikey=${apiKey}`, { headers: { "Accept": "Application/json" } });
+        const ipBaseData = await resolved.json();
+        clockData.city = ipBaseData.data.location.city.name_translated;
+        clockData.countryAbbreviated = ipBaseData.data.location.country.alpha2;
+    }
+    catch (error) {
+        console.log("this is the APILocation fetch error: ", error);
+    }
+}
+
 
 
 const prepareWelcomeMessage = () => {
-    const hours = apiTimeData.hours;
+    const hours = clockData.hours;
     let welcomeMessage = "";
     if (hours >= 5 && hours < 12) {
         welcomeMessage = "Good morning";
@@ -44,8 +58,11 @@ const prepareWelcomeMessage = () => {
 const displayClock = async () => {
     await apiTime();
     document.querySelector(".clock__welcomeMessage").innerText = prepareWelcomeMessage();
-    document.querySelector(".clock__time").innerText = `${apiTimeData.hours}:${apiTimeData.minutes}`;
-    document.querySelector(".clock__timezone").innerText = apiTimeData.timezoneAbbreviation;
+    document.querySelector(".clock__time").innerText = `${clockData.hours}:${clockData.minutes}`;
+    document.querySelector(".clock__timezone").innerText = clockData.timezoneAbbreviation;
+    /* await apiLocation();
+    document.querySelector(".clock__city").innerText = clockData.city;
+    document.querySelector(".clock__country").innerText = clockData.countryAbbreviated; */
 }
 
 displayClock();
@@ -69,3 +86,9 @@ const displayQuote = async () => {
 }
 
 displayQuote();
+
+
+
+
+
+
