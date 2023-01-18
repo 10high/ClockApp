5d7c66ac0723e82c4d9e3a3961c72a5cb53fb28f
10high <99827666+10high@users.noreply.github.com>
2023-01-18 13:36:39 +0100

    adds BG image functionality

--- src/app.js
+++ src/app.js
@@ -1,31 +1,9 @@
 import { displayClock } from "./components/clock";
 import { displayStats } from "./components/stats";
 import { displayQuote } from "./components/quote";
+import { clockData } from "./global/clockData";
 
 import { addButtonAction } from "./components/button";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -35,7 +13,21 @@ const initialFetchApisAndDisplayContent = async () => {
     await displayQuote();
 }
 
+const manageBackgroundImage = () => {
+    const hours = clockData.hours;
+    const backgroundElement = document.querySelector("main");
+    if (hours <= 5 || hours >= 18) {
+        backgroundElement.classList.remove("backgroundImage--day");
+        backgroundElement.classList.add("backgroundImage--night");
+    } else {
+        backgroundElement.classList.remove("backgroundImage--night");
+        backgroundElement.classList.add("backgroundImage--day");
+    }
+}
+
+
 initialFetchApisAndDisplayContent();
+manageBackgroundImage();
 addButtonAction();
 
 
