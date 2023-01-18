1eb113757635d795273d5f12cabdadb7da9beba5
10high <99827666+10high@users.noreply.github.com>
2023-01-18 12:25:51 +0100

    refactors and reorganizes button

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import { displayClock } from "./components/clock";
 import { displayStats } from "./components/stats";
 import { displayQuote } from "./components/quote";
 
+import { addButtonAction } from "./components/button";
+
 
 
 
@@ -34,34 +36,6 @@ const initialFetchApisAndDisplayContent = async () => {
 }
 
 initialFetchApisAndDisplayContent();
+addButtonAction();
 
 
-document.querySelector("#buttonMore").addEventListener("click", function () {
-    document.querySelector("#quoteContainer").classList.add("quote__container--shrink");
-    document.querySelector("#quoteContainer").classList.remove("quote__container--expand");
-    document.querySelector("#quote").classList.add("quote--hidden");
-    document.querySelector("#quote").classList.remove("quote--visible");
-
-    document.querySelector("#statsContainer").classList.add("stats__container--expand");
-    document.querySelector("#statsContainer").classList.remove("stats__container--shrink");
-    document.querySelector("#stats").classList.add("stats--visible");
-    document.querySelector("#stats").classList.remove("stats--hidden");
-
-    document.querySelector("#buttonLess").hidden = false;
-    document.querySelector("#buttonMore").hidden = true;
-
-    document.querySelector("#buttonLess").addEventListener("click", function () {
-        document.querySelector("#quoteContainer").classList.remove("quote__container--shrink");
-        document.querySelector("#quoteContainer").classList.add("quote__container--expand");
-        document.querySelector("#quote").classList.remove("quote--hidden");
-        document.querySelector("#quote").classList.add("quote--visible");
-
-        document.querySelector("#statsContainer").classList.remove("stats__container--expand");
-        document.querySelector("#statsContainer").classList.add("stats__container--shrink");
-        document.querySelector("#stats").classList.remove("stats--visible");
-        document.querySelector("#stats").classList.add("stats--hidden");
-
-        document.querySelector("#buttonMore").hidden = false;
-        document.querySelector("#buttonLess").hidden = true;
-    })
-})
--- src/components/button.js
+++ src/components/button.js
@@ -1,1 +1,50 @@
 
+
+
+const buttonElementsManager = {
+    buttonElementIds: ["buttonMore", "quoteContainer", "quote", "statsContainer", "stats", "buttonLess"],
+    buttonElements: {},
+    getElements() {
+        for (let elementId of this.buttonElementIds) {
+            this.buttonElements[elementId] = document.getElementById(elementId);
+        }
+    }
+}
+
+const buttonMoreAction = () => {
+    buttonElementsManager.buttonElements.quoteContainer.classList.add("quote__container--shrink");
+    buttonElementsManager.buttonElements.quoteContainer.classList.remove("quote__container--expand");
+    buttonElementsManager.buttonElements.quote.classList.add("quote--hidden");
+    buttonElementsManager.buttonElements.quote.classList.remove("quote--visible");
+
+    buttonElementsManager.buttonElements.statsContainer.classList.add("stats__container--expand");
+    buttonElementsManager.buttonElements.statsContainer.classList.remove("stats__container--shrink");
+    buttonElementsManager.buttonElements.stats.classList.add("stats--visible");
+    buttonElementsManager.buttonElements.stats.classList.remove("stats--hidden");
+
+    buttonElementsManager.buttonElements.buttonLess.hidden = false;
+    buttonElementsManager.buttonElements.buttonMore.hidden = true;
+}
+
+const buttonLessAction = () => {
+    buttonElementsManager.buttonElements.quoteContainer.classList.remove("quote__container--shrink");
+    buttonElementsManager.buttonElements.quoteContainer.classList.add("quote__container--expand");
+    buttonElementsManager.buttonElements.quote.classList.remove("quote--hidden");
+    buttonElementsManager.buttonElements.quote.classList.add("quote--visible");
+
+    buttonElementsManager.buttonElements.statsContainer.classList.remove("stats__container--expand");
+    buttonElementsManager.buttonElements.statsContainer.classList.add("stats__container--shrink");
+    buttonElementsManager.buttonElements.stats.classList.remove("stats--visible");
+    buttonElementsManager.buttonElements.stats.classList.add("stats--hidden");
+
+    buttonElementsManager.buttonElements.buttonMore.hidden = false;
+    buttonElementsManager.buttonElements.buttonLess.hidden = true;
+}
+
+
+export const addButtonAction = () => {
+    buttonElementsManager.getElements();
+    buttonElementsManager.buttonElements.buttonMore.addEventListener("pointerdown", buttonMoreAction);
+    buttonElementsManager.buttonElements.buttonLess.addEventListener("pointerdown", buttonLessAction);
+
+}
