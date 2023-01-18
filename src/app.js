37ee61cb49e97156f07ed965981ca6c0794bc798
10high <99827666+10high@users.noreply.github.com>
2023-01-18 10:36:13 +0100

    adds button more functionality

--- src/app.js
+++ src/app.js
@@ -36,4 +36,14 @@ const initialFetchApisAndDisplayContent = async () => {
 initialFetchApisAndDisplayContent();
 
 
+document.querySelector("#buttonMore").addEventListener("click", function () {
+    document.querySelector("#quoteContainer").classList.add("quote__container--shrink");
+    document.querySelector("#quoteContainer").classList.remove("quote__container--expand");
+    document.querySelector("#quote").classList.add("quote--hidden");
+    document.querySelector("#quote").classList.remove("quote--visible");
 
+    document.querySelector("#statsContainer").classList.add("stats__container--expand");
+    document.querySelector("#statsContainer").classList.remove("stats__container--shrink");
+    document.querySelector("#stats").classList.add("stats--visible");
+    document.querySelector("#stats").classList.remove("stats--hidden");
+})
