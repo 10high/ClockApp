e97c8a92468e7a25dfe3760098c22c3020b6c79d
10high <99827666+10high@users.noreply.github.com>
2023-01-18 10:46:37 +0100

    adds button less functionality

--- src/app.js
+++ src/app.js
@@ -46,4 +46,22 @@ document.querySelector("#buttonMore").addEventListener("click", function () {
     document.querySelector("#statsContainer").classList.remove("stats__container--shrink");
     document.querySelector("#stats").classList.add("stats--visible");
     document.querySelector("#stats").classList.remove("stats--hidden");
+
+    document.querySelector("#buttonLess").hidden = false;
+    document.querySelector("#buttonMore").hidden = true;
+
+    document.querySelector("#buttonLess").addEventListener("click", function () {
+        document.querySelector("#quoteContainer").classList.remove("quote__container--shrink");
+        document.querySelector("#quoteContainer").classList.add("quote__container--expand");
+        document.querySelector("#quote").classList.remove("quote--hidden");
+        document.querySelector("#quote").classList.add("quote--visible");
+
+        document.querySelector("#statsContainer").classList.remove("stats__container--expand");
+        document.querySelector("#statsContainer").classList.add("stats__container--shrink");
+        document.querySelector("#stats").classList.remove("stats--visible");
+        document.querySelector("#stats").classList.add("stats--hidden");
+
+        document.querySelector("#buttonMore").hidden = false;
+        document.querySelector("#buttonLess").hidden = true;
+    })
 })
