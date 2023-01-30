1bab58e243b34b45dee68b8d3b6e681b8b6d3160
10high <99827666+10high@users.noreply.github.com>
2023-01-30 17:42:10 +0100

    change from serverless function to direct env var access

--- src/api/location.js
+++ src/api/location.js
@@ -2,7 +2,8 @@ import { clockData } from "../global/clockData";
 
 
 export const apiLocation = async () => {
-    const resolved = await fetch("/.netlify/functions/apisecret");
+    const apiSecret = process.env.API_SECRET;
+    const resolved = await fetch(`https://api.ipbase.com/v2/info?apikey=${apiSecret}`);
     const ipBaseData = await resolved.json();
     clockData.city = ipBaseData.data.location.city.name_translated;
     clockData.countryAbbreviated = ipBaseData.data.location.country.alpha2;
